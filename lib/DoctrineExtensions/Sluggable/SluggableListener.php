6bf73bbe9e70d1e473a3767e8d02a2c6e664b509
Andy Stanberry <andystanberry@gmail.com>
2010-06-15 09:43:59 -0400

    Refactor, add support for preupdate on null

--- lib/DoctrineExtensions/Sluggable/SluggableListener.php
+++ lib/DoctrineExtensions/Sluggable/SluggableListener.php
@@ -18,27 +18,65 @@ class SluggableListener
     public function __construct(EventManager $evm)
     {
         $evm->addEventListener(Events::prePersist, $this);
+        $evm->addEventListener(Events::preUpdate, $this);
     }
 
     /**
+     * preUpdate
+     *
+     * @param EventArgs $e Event
+     */
+    public function preUpdate(EventArgs $e)
+    {
+        $sluggable = $this->getSluggable($e);
+
+        if (isset($sluggable) && $sluggable->getSlug() === null) {
+            $this->getGenerator($e)->process($sluggable);
+            $changeset = $e->getDocumentChangeSet();
+            $changeset[$sluggable->getSlugFieldName()] = array(null, $sluggable->getSlug());
+        }
+    }
+    /**
      * prePersist
-     * 
+     *
      * @param EventArgs $e Event
      */
     public function prePersist(EventArgs $e)
     {
-		if ($e instanceof ORMLifecycleEventArgs) {
-			$entity = $e->getEntity();
-			if ($entity instanceof Sluggable) {
-		        $generator = new SlugGenerator($e->getEntityManager());
-		        $generator->process($entity);
-		    }
-		} else if ($e instanceof ODMLifecycleEventArgs){
-			$document = $e->getDocument();
-			if ($document instanceof Sluggable) {
-		        $generator = new SlugGenerator($e->getDocumentManager());
-		        $generator->process($document);
-		    }
-		}
+        $sluggable = $this->getSluggable($e);
+
+        if (isset($sluggable)) {
+            $this->getGenerator($e)->process($sluggable);
+            $changeset = $e->getDocumentChangeSet();
+            $changeset[$sluggable->getSlugFieldName()] = array(null, $sluggable->getSlug());
+        }
+    }
+
+    protected function getSluggable(EventArgs $e) {
+        // ORM Entities
+        if ($e instanceof ORMLifecycleEventArgs) {
+            $entity = $e->getEntity();
+            if ($entity instanceof Sluggable) {
+                return $entity;
+            }
+        }
+
+        // ODM Documents
+        if ($e instanceof ODMLifecycleEventArgs){
+            $document = $e->getDocument();
+            if ($document instanceof Sluggable) {
+                return $document;
+            }
+        }
+
+        return null;
+    }
+
+    protected function getGenerator(EventArgs $e) {
+     	  if ($e instanceof ORMLifecycleEventArgs) {
+            return new SlugGenerator($e->getEntityManager());
+		  } else if ($e instanceof ODMLifecycleEventArgs){
+            return new SlugGenerator($e->getDocumentManager());
+        }
     }
 }
